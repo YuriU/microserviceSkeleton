495ed4d0574293265fdfb176d64f96568e094b4c
YuriU <jerryhire8@gmail.com>
2020-05-11 20:54:19 +0300

    Removed extra references

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,15 +1,10 @@
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Autofac.Extensions.DependencyInjection;
 using Data.Repository.EF;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace WebApi
 {
